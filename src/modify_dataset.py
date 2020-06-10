6229e9de4679ed521293921d02428d3ee81c5733
rafelps <rafel1995@gmail.com>
2020-06-10 12:07:22 +0200

    Correct bug in dataset

--- src/modify_dataset.py
+++ src/modify_dataset.py
@@ -3,7 +3,7 @@ import json
 from pizza import Pizza
 
 if __name__ == '__main__':
-    with open('../data/old_pizzas.json', 'r', encoding='utf-8') as f:
+    with open('../data/old_pizzas_v2.json', 'r', encoding='utf-8') as f:
         pizzas = json.load(f)
 
     pizzas = [Pizza(pizza['dough'], pizza['sauce'], pizza['toppings'], pizza['recipe'], pizza['name']) for pizza in pizzas]
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     for pizza in pizzas:
         pizza.set_recipe()
     
-    with open('../data/pizzas23.json', 'w', encoding='utf-8') as f:
+    with open('../data/pizzas9.json', 'w', encoding='utf-8') as f:
         json.dump(pizzas, f, default=lambda obj: obj.__dict__, indent=4)
